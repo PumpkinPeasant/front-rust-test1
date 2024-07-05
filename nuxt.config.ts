2bca79c3ba6d36db31d221b812529010bcaeb233
Anton <anton.n@prima-inform.ru>
2024-07-05 15:42:26 +0300

    added vuetify

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,6 +3,8 @@ import vuetify from "vite-plugin-vuetify";
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
+  css: ["@/assets/main.scss"],
+  build: { transpile: ["vuetify"] },
   modules: [
     async (options, nuxt) => {
       nuxt.hooks.hook("vite:extendConfig", (config) => {
--- plugins/vuetify.ts
+++ plugins/vuetify.ts
@@ -1,9 +1,16 @@
 import { createVuetify } from "vuetify";
+import { md3 } from "vuetify/blueprints";
+import { ru } from 'vuetify/locale'
 
 export default defineNuxtPlugin((app) => {
   const vuetify = createVuetify({
     ssr: true,
+    blueprint: md3,
     theme: {},
+    locale:{
+      locale: 'ru',
+      messages: { ru },
+    },
   });
 
   app.vueApp.use(vuetify);
